d57fd20f8163b1d1e42ed8aa25882dae2dbe2697
Nimsrules <nimsrules.mfans@gmail.com>
2019-02-28 16:39:52 +0530

    *Date field set *Toggle event description *localStorage check *Max lengths added

--- js/app.js
+++ js/app.js
@@ -10,32 +10,37 @@ document.addEventListener('DOMContentLoaded', function(){
 			return `
 			<li>
 				<a href="#">
-					<div class="content">
-						<h6>${event.eventName}</h6>
-						<ul class="meta">
-							<li>${event.eventLocation}</li>
-							<li>${event.eventType}</li>
-							<li>${event.eventDate}</li>
-						</ul>
-						<small>${event.eventDescription}</small>
-					</div>
-					<span class="button">View</span>
+				<div class="content">
+					<h6>${event.eventName}</h6>
+					<ul class="meta">
+						<li>${event.eventLocation}</li>
+						<li>${event.eventType}</li>
+						<li>${event.eventDate}</li>
+					</ul>
+					<small>${event.eventDescription}</small>
+				</div>
+				<span class="button">View</span>
 				</a>
 			</li>
 			`;
 		}
 
 		//Iterating over the JSON data and adding it using the above declared literal
-		var htmlString = '<ul>';
 		localStorage.getItem('events') ? data = JSON.parse(localStorage.getItem('events')) : data;
+		var htmlString = '<ul>';
 		data.forEach((event, idx) => {
-		    var markup = createEvents(event, idx);
-		    htmlString += markup;
+			var markup = createEvents(event, idx);
+			htmlString += markup;
 		});
 		htmlString = htmlString + '</ul>';
 		events.innerHTML = "<h2>Events</h2>" + htmlString + "<div class='text-center'><a href='#'' class='add-event button button--filled'>New Event</a></div>";
+		setTimeout(function(){
+			var newAddedEvent = document.getElementsByClassName('new');
+			if(newAddedEvent.length > 1){
+				document.querySelectorAll('.new').classList.remove('new');
+			}
+		}, 500);
 	}
-
 	populateEvents();
 
 	/*------------------------------------------------------------------------------------
@@ -53,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 		data.push(newEvent);
 		localStorage.setItem('events', JSON.stringify(data));
+		document.querySelector('.events>ul>li:last-child').classList.add('new');
 	}
 
 	//Click event of the Add New Event button
@@ -75,5 +81,19 @@ document.addEventListener('DOMContentLoaded', function(){
 	addEventCancelButton.addEventListener('click', function(){
 		events.style.display = 'block';
 		eventsForm.style.display = 'none';
+	});
+
+	//Toggling the description of single event
+	let singleEvent = document.querySelectorAll('.events>ul>li>a');
+	singleEvent.forEach((s) => {
+		s.addEventListener('click', function(e){
+			var thisDescription = this.querySelector('small');
+			if(window.getComputedStyle(thisDescription).display === 'block'){
+				thisDescription.style.display = 'none';
+			}else{
+				thisDescription.style.display = 'block';
+			}
+			e.preventDefault();
+		});
 	});
 });
